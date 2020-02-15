e74655ecf5edae286015a9614e5747c20f42eb9c
amiko <amichai@mantinband.com>
2020-02-15 12:22:36 +0200

    algo twitches

--- algorithm.py
+++ algorithm.py
@@ -22,15 +22,17 @@ def compute_board_score(board, key):
         for j in range(0, Board.BOARD_SIZE):
             if board.get_value(i, j) == board.get_value(i + 1, j):
                 score += board.get_value(i, j) ** 2
-                score *= 1.5
+                score *= 1.7
+            elif board.get_value(i, j) > board.get_value(i+1, j):
+                score *= 0.9
 
     if key == Keys.ARROW_DOWN:
-        score *= 2**board.number_of_merged_tiles()
-        score *= 2.5 if board.get_value(Board.BOARD_SIZE-1, 0) == Board.EMPTY_TILE else 1.5
+        score *= 2 ** board.number_of_merged_tiles()
+        score *= 2.5 if board.get_value(Board.BOARD_SIZE - 1, 0) == Board.EMPTY_TILE else 1.5
 
     if not board.last_row_has_empty_spot() and key == Keys.ARROW_LEFT:
         if board.biggest_in_corner():
-            score *= 1.1
+            score *= 1.2
         else:
             score *= 0.5
 
